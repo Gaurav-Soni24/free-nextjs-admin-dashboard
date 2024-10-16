40daaff2c6452fb0b1d27cb797df162b5d766735
Gaurav-Soni24 <sonigaurav2021@gmail.com>
2024-10-17 01:02:54 +0530

    improve

--- src/components/firebase/firebase.tsx
+++ src/components/firebase/firebase.tsx
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAeoqVd-hB84IwdJjFYuVBFk0bz5ngDwPg",
-  authDomain: "airwatch-48b0e.firebaseapp.com",
-  projectId: "airwatch-48b0e",
-  storageBucket: "airwatch-48b0e.appspot.com",
-  messagingSenderId: "998560344282",
-  appId: "1:998560344282:web:0854d868f68c625c2a5de3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
